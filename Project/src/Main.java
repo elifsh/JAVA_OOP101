d48ad77494e346cc55037ac523e4bf80200655e8
Elif Keleş <87431427+elifsh@users.noreply.github.com>
2022-03-10 23:43:55 +0300

    Delete Project/src directory

--- Project/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Fighter f1 = new Fighter("Elif", 20, 100, 60, 0);
-        Fighter f2 = new Fighter("Efe", 25, 75, 70, 0);
-        Ring r = new Ring(f1, f2, 60, 75);
-        r.run();
-    }    
-}
